1943bd054ecef7ac9b5cdb8e19264628f8405048
Abdul-Rasith <mscrasith@gmail.com>
2024-04-01 12:12:50 +0530

    Upload Day-7 Task

--- script1.js
+++ script1.js
@@ -10,15 +10,18 @@ request.onload = function(){
 // a.Get all the countries from Asia continent /region using Filter function
 var resul_asia = res.filter((ele)=>ele.region == "Asia");
 console.log(resul_asia);
+
 //b. Get all the countries with a population of less than 2 lakhs using Filter function
 let popu = res.filter((elem)=>elem.population<200000);
 console.log(popu);
-//c.Print the following details name, capital, flag, using forEach function
 
-Object.keys(res).forEach((ele)=>console.log(ele,res[ele].name+" "+res[ele].capital+" "+res[ele].flag))
+//c.Print the following details name, capital, flag, using forEach function
+Object.keys(res).forEach((ele)=>console.log(ele,`${res[ele].name.common} ${res[ele].capital} ${res[ele].flag}`))
+
 //d.Print the total population of countries using reduce function
 let total_popu = res.reduce((acc,cv)=>acc+cv.population,0);
 console.log(total_popu);
+
 //e. Print the country that uses US dollars as currency.
 for(var i=0; i<res.length; i++){
     console.log(res[i].currencies)
